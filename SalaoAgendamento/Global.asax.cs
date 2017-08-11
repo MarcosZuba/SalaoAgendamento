73337ca450e0668b388992720e9839e027e2f41d
MarcosZuba <z-marcos-z@hotmail.com>
2017-08-11 16:37:59 -0300

    Update Global.asax.cs

--- SalaoAgendamento/Global.asax.cs
+++ SalaoAgendamento/Global.asax.cs
@@ -13,6 +13,7 @@ namespace SalaoAgendamento
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //Zuba
         }
     }
 }
